3ba77d9ee5fc420763f04a28de0d6b19cbb0230b
MrCoderBro <jaydenweijun@gmail.com>
2026-01-27 14:43:05 +0800

    Commit

--- demo-app/src/App.jsx
+++ demo-app/src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-import Calendar from "./components/Calendar";
+import Calendar from "./components/Calendar"
 
 
+function App(){
 
-function App() {  return (
+  return(
+
     <div className="App">
-      <h1>React Calendar Demo</h1>
-      <Calendar />
+
+      <Calendar/>
+
     </div>
-  );
+
+  )
+
 }
+
 
 export default App;
--- demo-app/src/components/Calendar.jsx
+++ demo-app/src/components/Calendar.jsx
@@ -1,19 +1,68 @@
 import React from "react";
 
+import FullCalendar from "@fullcalendar/react";
+
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
+
+import * as bootstrap from "bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./index.css";
+
 function Calendar() {
+  //Event setting
+
+  //details inside
+
+  const events = [
+    {
+      title: "The title",
+
+      start: "2026-01-04T08:00:00",
+
+      end: "2026-01-04T010:00:00",
+    },
+  ];
+
   return (
-    <div>
-      <FullCalendar
-        initialView="dayGridMonth"
-        plugins={[dayGridPlugin, interactionPlugin, timegridPlugin]}
-        headerToolbar={{
-          start: "dayGridMonth,timeGridWeek,timeGridDay",
-          center: "title",
-          end: "prev,next today",
-        }}
-        editable={true}
-      />
-    </div>
+    <>
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView={"dayGridMonth"}
+          headerToolbar={{
+            start: "dayGridMonth, timeGridWeek, timeGridDay", // will normally be on the left. if RTL, will be on the right
+
+            center: "title",
+
+            end: "today prev,next", // will normally be on the right. if RTL, will be on the left
+          }}
+          // height={"90vh"}
+          events={events}
+          eventDidMount={(info) => {
+            return new bootstrap.Popover(info.el, {
+              title: info.event.title,
+
+              placement: "auto",
+
+              trigger: "hover",
+
+              customClass: "popoverStyle",
+
+              content:
+                "<p>Please subscribe <strong>Bootstrap popover</strong>.</p>",
+
+              html: true,
+            });
+          }}
+        />
+      </div>
+    </>
   );
 }
 
